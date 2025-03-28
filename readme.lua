39372b298391de1e08a98bbce8d62aef667df9e2
Abraham Kiplagat <kurerelagat01@.com>
2025-03-28 18:54:53 +0300

    readme

---
+++ readme.lua
@@ -0,0 +1,12 @@
+## variables in lua are:
+local a, b, c, d
+a = "Abraham Kiplagat Kurere"
+b = 20
+c = "Male"
+d = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+print("Welcome to " .. a .. ", I am " .. b .. " years old " .. c) -- Corrected concatenation
+
+for i, v in pairs(d) do
+    print(i, v) -- Corrected loop variable name
+end
